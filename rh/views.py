04cb3e7a69b065d3be2378d0adf68bdab6136ffb
Gerhard Neto <neto.gb@outlook.com>
2022-07-07 20:09:16 -0300

    conexão com mongodb

--- rh/views.py
+++ rh/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
-
-# Create your views here.
 from django.http import HttpResponse
 
+import pprint
+from pymongo import MongoClient
 
 def index(request):
+    client = MongoClient("mongodb+srv://mongo:mongo@cluster0.ipvq6.mongodb.net/?retryWrites=true&w=majority")
+    
+    db = client.rh
+    print('db', db)
+    print(db.list_collection_names())
+
+    pontos = db.ponto
+
+    pprint.pprint(pontos.find_one())
+
     return HttpResponse("<h1>Hello HR-TECH!</h1>")
